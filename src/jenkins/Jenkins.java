8ae4782c3f63e658063f8b0c4b1ba4c400e56fa2
joalro2122daw2 <15584556.clot@fje.edu>
2022-01-22 18:40:50 +0100

    Primera modificació de la prova d'IC amb Jenkins

--- src/jenkins/Jenkins.java
+++ src/jenkins/Jenkins.java
@@ -2,10 +2,10 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package jenkins;
+package jenkins
 /**
  *
- * @author dc1617daw2
+ * @author joalro2122daw2
  */
 import java.io.*;
 public class Jenkins {
